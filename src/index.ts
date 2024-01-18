3d71dd4f794aee752eb8c37e43dbebb118ff3dd5
afeiship <1290657123@qq.com>
2024-01-19 07:41:53 +0800

    feat: hasSw

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-const swRuntime = (): void => {
-  console.log('hello');
-};
+interface ISwRuntimeOptions {
+  force?: boolean;
+}
 
-export default swRuntime;
+class SwRuntime {
+  private options: ISwRuntimeOptions;
+
+  constructor(inOptions: ISwRuntimeOptions) {
+    this.options = inOptions;
+  }
+
+  has() {
+    if (this.options.force) {
+      return 'serviceWorker' in navigator;
+    } else {
+      return (
+        'serviceWorker' in navigator &&
+        (window.location.protocol === 'https:' ||
+          window.location.hostname === 'localhost' ||
+          window.location.hostname.indexOf('127.') === 0)
+      );
+    }
+  }
+}
+
+export default SwRuntime;
